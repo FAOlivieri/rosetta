47f0150f6e99ca20b81b19aa6e6872b73eaf1007
FAOlivieri <federicoaolivieri@gmail.com>
2024-01-30 13:09:46 -0300

    Queue test file

--- source/test/protocols/bootcamp/QueueTest.cxxtest.hh
+++ source/test/protocols/bootcamp/QueueTest.cxxtest.hh
@@ -7,7 +7,7 @@
 // (c) For more information, see http://www.rosettacommons.org. Questions about this can be
 // (c) addressed to University of Washington CoMotion, email: license@uw.edu.
 
-/// @file  t/e/s/t///p/r/o/t/o/c/o/l/s///b/o/o/t/c/a/m/p/QueueTest.cxxtest.hh
+/// @file  protocols/bootcamp/QueueTest.cxxtest.hh
 /// @brief  lab3 test
 /// @author FAOlivieri (federicoaolivieri@gmail.com)
 
